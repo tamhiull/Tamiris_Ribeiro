3735d1a7ac35c3a62fb17fd78bd573b27bb9c440
tamhiull <119531125+tamhiull@users.noreply.github.com>
2026-02-01 16:29:56 -0400

    fix: ensure tailwind css is loaded

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
+import "./index.css";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
-import "./index.css";
+
 
 createRoot(document.getElementById("root")!).render(
   <HashRouter>
