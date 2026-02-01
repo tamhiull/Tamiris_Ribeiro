2dfbbf1d16f9204cab4dca0aac13723734dcd438
tamhiull <119531125+tamhiull@users.noreply.github.com>
2026-02-01 15:11:04 -0400

    fix: base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
   base: "/Tamiris_Ribeiro/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
 });
